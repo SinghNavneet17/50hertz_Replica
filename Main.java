d630aeb81a0df495b8b753fbf0b8e29e22c15731
SinghNavneet17 <singhnavneet.1711@gmail.com>
2025-06-28 19:01:31 +0530

    Commit-2 by navneet

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main{
 
 public static void main(String[] args) {
 
-    System.out.println("This is the first commit from Navneet's Branch");
+    System.out.println("This is the second commit from Navneet's Branch");
 
 }
     
