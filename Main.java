1445a10a36cedec87f9e290edc5cc1b5a97b7550
Navneet Singh <singhnavneet.1711@gmail.com>
2025-06-28 19:20:54 +0530

    commit by Vish

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
 
 public static void main(String[] args) {
-    System.out.println("This is the second commit from Navneet's Branch");
+    System.out.println("This is the second commit from Vish's Branch");
 }  
 }
