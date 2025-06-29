cc47d868db0ea0e6a085b0bfc380a01133dff174
SinghNavneet17 <singhnavneet.1711@gmail.com>
2025-06-30 00:05:32 +0530

    extension Merge

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
 
 public static void main(String[] args) {
-    System.out.println("************************** Navneet's Branch ***********************");
-    System.out.println("**************************This is the extension commit*************");
+    System.out.println("********************Palo's_Branch*******This is extension commit******************");
+    System.out.println("**************************This is the extension commit by Navneet*************");
 }  
 }
