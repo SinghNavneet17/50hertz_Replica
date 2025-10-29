23dfc2333b7a4f6c594b53b3091c0135c8cb126a
Navneet Singh <singhnavneet.1711@gmail.com>
2025-10-30 00:31:01 +0530

    Update print statements in Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
 public static void main(String[] args) {
-    System.out.println("********************Palo's_Branch*******This is extension commit******************");
-    System.out.println("**************************Special commit by Navneet*************");
+    System.out.println("********************This is the feature branch******************");
+    System.out.println("**************************Random commit*************");
    System.out.println("hey how are you");
     System.out.println("Great Day");
 }
