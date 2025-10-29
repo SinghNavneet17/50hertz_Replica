4cf656a3e6e568e0c02d720072a8951136861165
SinghNavneet17 <singhnavneet.1711@gmail.com>
2025-10-30 00:45:14 +0530

    used stash pop here

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main{
 public static void main(String[] args) {
     System.out.println("********************Palo's_Branch*******This is extension commit******************");
-    System.out.println("**************************Special commit by Navneet*************");
-    System.out.println("feeling delighted");
-    System.out.println("Great Day");
+    System.out.println("**************************Random commit*************");
+    System.out.println("hey how are you doing today ?");
+    System.out.println("hope you are doing great today");
 }
 }
