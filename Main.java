208cc3c1da235b8c1d87706ca7200f72e2711ba6
SinghNavneet17 <singhnavneet.1711@gmail.com>
2025-11-10 23:18:54 +0530

    this is dev one branch first commit

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
 public static void main(String[] args) {
-    System.out.println("********************Palo's_Branch*******This is extension commit******************");
-    System.out.println("**************************Random commit*************");
+    System.out.println("********************DEVELOPER_ONE******************");
+    System.out.println("**************************Navneet Singh*************");
     System.out.println("hey how are you doing today ?");
     System.out.println("hope you are doing great today");
 }
